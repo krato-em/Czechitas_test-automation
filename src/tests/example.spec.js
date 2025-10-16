8c0ddf197e0eef82482de62d8d605511b955e584
Emilie Kratochvilova <Emilie.Kratochvilova@solarwinds.com>
2025-10-16 12:34:21 +0200

    Code from Lesson 3

--- src/tests/example.spec.js
+++ src/tests/example.spec.js
@@ -15,3 +15,54 @@ test("lesson 2", async ({ page }) => {
     await page.getByLabel("Email").screenshot({ path: "by_label.png"});
     console.log(await page.title());
 });
+
+
+test("lesson 3", async ({ page }) => {
+    
+    await page.goto("/prihlaseni");
+
+    const headingLocator = await page.getByRole("heading", {level: 1});
+    const headingText = await headingLocator.textContent();
+    console.log(headingText);
+    const emailField = await page.getByLabel("Email");
+    console.log("Is email field visible? " + await emailField.isVisible());
+    console.log("Is email field enabled? " + await emailField.isEnabled());
+
+    const forgotPassword = page.getByText("Zapomněli jste své heslo?");
+    console.log("Forgot password href: " + await forgotPassword.getAttribute("href"));
+
+    const passwordField = page.getByLabel("Heslo");
+
+
+    const loginBtn = page.getByRole("button", { name: "Přihlásit" });
+    console.log("Login button text: " + await loginBtn.textContent());
+
+    await emailField.fill("da-app.admin@czechitas.cz");
+    await passwordField.fill("Czechitas123");
+    await loginBtn.click({
+        button: "left",
+        clickCount: 1,
+        delay: 100
+    });
+
+    const userName = page.locator(".navbar-right").locator("strong");
+    console.log("Logged in user: " + await userName.textContent());
+});
+
+
+test("Lesson-3: BreakoutRoom", async ({ page }) => {
+    
+    await page.goto("/prihlaseni");
+
+    const btnLogin = await page.getByRole('button', { name: 'Přihlásit' });
+    // await btnLogin.waitFor();
+    //  await btnLogin.evaluate()
+    console.log("Button text: " + await btnLogin.textContent());
+
+    const passwordField = page.getByLabel("Heslo");
+    console.log("Je videt? " + await passwordField.isVisible());
+    console.log("Je povoleno? " + await passwordField.isEnabled());
+
+    btnLogin.click();
+    
+});
